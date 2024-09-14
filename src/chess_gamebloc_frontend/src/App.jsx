93db6f030a65b4748a1ec5637df5462bf546f6f2
Sukuna <ajeemmanuel221@gmail.com>
2024-09-14 06:57:54 +0100

    added the material/UI for the chessboard representation

--- src/chess_gamebloc_frontend/src/App.jsx
+++ src/chess_gamebloc_frontend/src/App.jsx
@@ -1,31 +1,12 @@
 import { useState } from 'react';
-import { chess_gamebloc_backend } from 'declarations/chess_gamebloc_backend';
+import { Container } from '@mui/material';
+import Game from "./Game"
+
 
 function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    chess_gamebloc_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
-  return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
+  <Container>
+    <Game />
+  </Container>
 }
 
 export default App;
---
+++ src/chess_gamebloc_frontend/src/components/CustomDialog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export const CustomDialog = (open, children, title, contentText, handleContinue) => {
+  return (
+    <Dialog open={open}>
+        
+    </Dialog>
+  )
+}
