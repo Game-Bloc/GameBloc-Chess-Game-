5bdf75d00505ec1456b41bf4488bd6ef2d76c6bb
Sukuna <ajeemmanuel221@gmail.com>
2024-09-20 21:45:37 +0100

    ripped the code all off, the rust test here implemented

--- src/chess_gamebloc_backend/src/lib.rs
+++ src/chess_gamebloc_backend/src/lib.rs
@@ -1,12 +1,12 @@
 
 use candid::Principal;
-use ic_cdk::{api::call::ManualReply, query, update};
+use ic_cdk::{api::call::ManualReply, init, query, update};
 use serde::Serialize;
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 use std::collections::BTreeMap;
 
 type IdStore = BTreeMap<String, Principal>;
-type ProfileStore = BTreeMap<Principal, Profile>;
+type ProfileStore = BTreeMap<Principal, Profile>; // the 'profile' here, is it the same with the struct name
 
 #[derive(Clone, Debug, Default, candid::CandidType, candid::Deserialize, Serialize)]
 struct Profile {
@@ -18,6 +18,9 @@ struct Profile {
 thread_local! {
     static PROFILE_STORE: RefCell<ProfileStore> = RefCell::default();
     static ID_STORE: RefCell<IdStore> = RefCell::default(); // is it the same IdStore above we are calling here also
+    // for the counter function trial
+    static COUNTER: RefCell<candid::Nat> = RefCell::new(candid::Nat::from(0u8));
+    static OWNER: Cell<Principal> = Cell::new(Principal::from_slice(&[]));
 }
 
 #[query(name = "getSelf", manual_reply = true)]
@@ -55,7 +58,7 @@ fn update(profile: Profile) {
     PROFILE_STORE.with(|profile_store| {
         profile_store.borrow_mut().insert(principal_id, profile);
     })
-} // I honestly don't knw what this code does
+} // I honestly don't knw what this code does... now I do :)
 
 #[query(manual_reply = true)]
 fn search(text: String) -> ManualReply<Option<Profile>> {
@@ -75,6 +78,36 @@ fn search(text: String) -> ManualReply<Option<Profile>> {
         ManualReply::one(None::<Profile>)
     })
 }
+
+// for the counter functions
+#[init] // the 'init' function is used when I want a function to run before main
+fn init() {
+    OWNER.with(|owner| owner.set(ic_cdk::api::caller()));
+}
+
+#[update]
+fn inc() {
+    ic_cdk::println!("{:?}", OWNER.with(|owner| owner.get()));
+    COUNTER.with(|counter| *counter.borrow_mut() += 1u64);
+}
+
+#[query(manual_reply = true)]
+fn read() -> ManualReply<candid::Nat> { // I understand that the 'ManualReply is used for only query-type of functions'
+    COUNTER.with(|counter| ManualReply::one(counter))
+}
+
+#[update]
+fn write (input: candid::Nat) {
+    COUNTER.with(|counter| *counter.borrow_mut() = input)
+}
+
+#[update(hidden = true)]
+fn update_hidden() {
+
+}
+
+#[query(hidden = true)]
+fn query_hidden() {}
 
 ic_cdk::export_candid!();
 
