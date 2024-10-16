2bedcc40050608714834873262d9a45fcc00352f
Sukuna <ajeemmanuel221@gmail.com>
2024-10-16 11:39:33 +0100

    updating on the context

--- src/chess_gamebloc_frontend/src/functions/context.ts
+++ src/chess_gamebloc_frontend/src/functions/context.ts
@@ -1,6 +1,14 @@
 
 import { createContext } from "react";
 
-export const profileContext = createContext(undefined);
+export interface contextProfile {
+    age : number,
+    principal : string | undefined,
+    name : string | undefined,
+    count : number | undefined,
+    description : string | undefined
+}
+
+export const profileContext = createContext<contextProfile | undefined>(undefined);
 
 
