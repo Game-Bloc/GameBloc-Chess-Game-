635bdac2927d6f5abc73309397854828d26ce0a2
Sukuna <ajeemmanuel221@gmail.com>
2024-10-21 23:32:43 +0100

    changed route

--- src/chess_gamebloc_frontend/src/LandingPage.tsx
+++ src/chess_gamebloc_frontend/src/LandingPage.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useAuth } from "./auth/use_auth_client";
+
 
 const LandingPage = () => {
+    const { isAuthenticated,login ,loginNFID} = useAuth();
   return (
-    <div>LandingPage</div>
+    <div>
+        <button onClick={() => {
+            console.log('login:', loginNFID);
+            login();
+        }}>Login with II</button>
+
+        <button onClick={() => {
+        console.log('login:', loginNFID);
+        loginNFID();
+        }}>Login with NFID</button>
+    </div>
   )
 }
 
