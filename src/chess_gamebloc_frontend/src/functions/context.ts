ea40b8ef2b02f161382555052bb73d58b7dd1b17
Sukuna <ajeemmanuel221@gmail.com>
2024-10-23 23:51:05 +0100

    git

--- src/chess_gamebloc_frontend/src/functions/context.ts
+++ src/chess_gamebloc_frontend/src/functions/context.ts
@@ -16,7 +16,7 @@ export function UseProfileContext () {
     const users = useContext(profileContext)
 
     if (users === undefined) {
-        throw new Error("must be wrapped with ")
+        throw new Error("must be wrapped with profileCo")
     }
 
     return users;
