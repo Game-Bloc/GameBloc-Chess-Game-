f0fff8a18302e83481923068eee01a1ecf627e09
Sukuna <ajeemmanuel221@gmail.com>
2024-11-13 11:44:41 +0100

    added the actor from useAuth to the useContext

--- src/chess_gamebloc_frontend/src/AppWrap.tsx
+++ src/chess_gamebloc_frontend/src/AppWrap.tsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import React from 'react'
 import ChessPage from './ChessPage'
 import { ContextProfile, profileContext } from './functions/context'
-
+import { useAuth } from './auth/use_auth_client'
 
 interface AppWrapProp {}
 const AppWrap = ({} : AppWrapProp) => {
 
+    const { whoamiActor } = useAuth()
+    const actorsAct = whoamiActor?.get_player
     const [profile] = useState<ContextProfile>({
         age: 0,
         principal: "Softwork",
         name: "Sukuna",
         count: 0,
         description: ""
-      })
+    })
   return (
     <>
     <profileContext.Provider value={profile}>
