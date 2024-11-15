96ba1fa1e5cc2e58f0efe9fcd5f152a03b1cbf8c
Sukuna <ajeemmanuel221@gmail.com>
2024-11-15 02:07:31 +0100

    created the get_profile_function

--- src/chess_gamebloc_frontend/src/AppWrap.tsx
+++ src/chess_gamebloc_frontend/src/AppWrap.tsx
@@ -7,21 +7,27 @@ import { useAuth } from './auth/use_auth_client'
 interface AppWrapProp {}
 const AppWrap = ({} : AppWrapProp) => {
 
-    const { whoamiActor } = useAuth()
-    const actorsAct = whoamiActor?.get_player
-    const [profile] = useState<ContextProfile>({
-        age: 0,
-        principal: "Softwork",
-        name: "Sukuna",
-        count: 0,
-        description: ""
-    })
+  const { whoamiActor } = useAuth()
+  const actorsAct = whoamiActor?.get_player
+  
+  const [profile] = useState<ContextProfile>({
+      age: 0,
+      principal: "Softwork",
+      name: "Sukuna",
+      count: 0,
+      description: ""
+  })
+
+  const get_Player_Profile = async () => {
+    
+  }
+
   return (
-    <>
+    
     <profileContext.Provider value={profile}>
         <ChessPage />
     </profileContext.Provider>
-    </>
+  
   )
 }
 
