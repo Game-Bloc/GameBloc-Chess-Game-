1b730046a795d11eaf200ca897973f106886a70a
Sukuna <ajeemmanuel221@gmail.com>
2024-12-09 15:23:29 +0100

    added the w_s to the backend

--- src/chess_gamebloc_backend/src/handler.rs
+++ src/chess_gamebloc_backend/src/handler.rs
@@ -13,18 +13,18 @@ pub struct AppMessage {
 
 impl AppMessage {
     fn candid_serialize (&self) -> Vec<u8> {
-        encode_one(&self).unwrap();
+        encode_one(&self).unwrap()
     }
 }
 
-pub fn on_open(args: OnMessageCallbackArgs) {
+pub fn on_open(args: OnOpenCallbackArgs) {
     let msg = AppMessage {
-        text: String("ping")
+        text: String::from("ping"),
     };
-    send_app_message(args.client_principal, new_msg)
+    send_app_message(args.client_principal, msg);
 }
 
-pub fn on_message ( args: OnMessageCallbackArgs ) {
+pub fn on_message(args: OnMessageCallbackArgs) {
     let app_msg: AppMessage = decode_one(&args.message).unwrap();
     let new_msg = AppMessage {
         text: String::from("ping"),
@@ -33,15 +33,15 @@ pub fn on_message ( args: OnMessageCallbackArgs ) {
     send_app_message(args.client_principal, new_msg)
 }
 
-pub fn send_app_message( client_principal: ClientPrincipal, msg: AppMessage ) {
+fn send_app_message(client_principal: ClientPrincipal, msg: AppMessage) {
     print(format!("Sending message: {:?}", msg));
     if let Err(e) = send(client_principal, msg.candid_serialize()) {
-        print("Could not send message: {}", e);
+        print(format!("Could not send message: {}", e));  // I adjusted this line by adding the 'format' and wrap the msg as one arg
     }
 }
 
 pub fn on_close( args: OnCloseCallbackArgs ) {
-    print(format!("Sending message:{:?}", args.client_principal));
+    print(format!("Client {} is disconnected", args.client_principal));
 }
 
 
--- src/chess_gamebloc_backend/src/lib.rs
+++ src/chess_gamebloc_backend/src/lib.rs
@@ -15,7 +15,7 @@ use tanton::tools::Searcher;
 // from the ic-cdk on icp 
 
 mod getrandom_fail;
-// mod handler;
+mod handler;
 
 type GameStore = BTreeMap<String, GameInternal>; // from cdk-chess
 type IdStore = BTreeMap<String, Principal>;
