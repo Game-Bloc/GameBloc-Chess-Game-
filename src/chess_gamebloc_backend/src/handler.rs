af4c5ff713afe20dd3eb6dd625fee161b1933fb7
Sukuna <ajeemmanuel221@gmail.com>
2024-12-09 15:14:34 +0100

    added the w_s to the backend

--- src/chess_gamebloc_backend/src/handler.rs
+++ src/chess_gamebloc_backend/src/handler.rs
@@ -29,7 +29,22 @@ pub fn on_message ( args: OnMessageCallbackArgs ) {
     let new_msg = AppMessage {
         text: String::from("ping"),
     };
+    print(format!("Received message: {:?}", app_msg));
+    send_app_message(args.client_principal, new_msg)
 }
+
+pub fn send_app_message( client_principal: ClientPrincipal, msg: AppMessage ) {
+    print(format!("Sending message: {:?}", msg));
+    if let Err(e) = send(client_principal, msg.candid_serialize()) {
+        print("Could not send message: {}", e);
+    }
+}
+
+pub fn on_close( args: OnCloseCallbackArgs ) {
+    print(format!("Sending message:{:?}", args.client_principal));
+}
+
+
 
 
 
