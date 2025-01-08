e57354181a1c4a6b7399793c5e892104ac2c610f
Sukuna <ajeemmanuel221@gmail.com>
2025-01-08 17:57:35 +0100

    updated the broadcast function

--- src/chess_gamebloc_frontend/src/Ws.mjs
+++ src/chess_gamebloc_frontend/src/Ws.mjs
@@ -9,7 +9,28 @@ const server = createServer()
 const wsServer = new WebSocketServer({ server })
 
 const port = 8000
-const connections = {}
+const connections = { }
+const users = { }
+
+const broadcast = () => {
+    Object.keys(connections).forEach(uuid => {
+        const connection = connections[uuid]
+        const message = JSON.stringify(users)
+
+        connection.send(message)
+    })
+}
+
+const handleMessage = (bytes, uuid) => {
+    const message = JSON.parse(bytes.toString())
+    const user = users[uuid]
+    user.state = message
+    console.log(message)
+}
+
+const handleClose = () => {
+
+}
 
 wsServer.on("connection", (connection, request) => {
     // ws://localhost:8000?Username=Emma
@@ -21,6 +42,14 @@ wsServer.on("connection", (connection, request) => {
 
     // broadcast of message
     connections[uuid] = connection
+
+    users[uuid] = {
+        username,
+        state: {}
+    }
+
+    connection.on("message", message => handleMessage( message, uuid ))
+    // connection.close("message", () => handleClose( uuid ))
 
 })
 
