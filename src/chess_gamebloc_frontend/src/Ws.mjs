851d98a6987b245014ed9a14b1ce68d951fbea93
Sukuna <ajeemmanuel221@gmail.com>
2025-01-08 03:49:04 +0100

    added the uuid

--- src/chess_gamebloc_frontend/src/Ws.mjs
+++ src/chess_gamebloc_frontend/src/Ws.mjs
@@ -1,18 +1,26 @@
 import { createServer } from 'http'
 import { WebSocketServer } from 'ws'
 import { v4 as uuidv4 } from "uuid"
+import url from 'node:url';
 
-const url = require('url')
+// import { URL } from 'url'
+// var url = require('url')
 const server = createServer()
 const wsServer = new WebSocketServer({ server })
 
 const port = 8000
+const connections = {}
 
-wsServer.on(connection, (connection, request) => {
+wsServer.on("connection", (connection, request) => {
     // ws://localhost:8000?Username=Emma
 
     const { username } = url.parse(request.url, true).query
+    const uuid = uuidv4()
     console.log(username)
+    console.log(uuid)
+
+    // broadcast of message
+    connections[uuid] = connection
 
 })
 
