0682c94d825758251a64263dddadc0fbd4b96c95
Sukuna <ajeemmanuel221@gmail.com>
2025-01-16 00:19:18 +0100

    bug fixed

--- src/chess_gamebloc_frontend/src/components/Home.jsx
+++ src/chess_gamebloc_frontend/src/components/Home.jsx
@@ -17,29 +17,33 @@ const renderCursors = (users) => {
 }
 
 export function Home({ username }) {
-    const WS_URL = `ws://127.0.0.1:8000`
-    const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
-      share: true,
-      queryParams: { username },
+  const WS_URL = `ws://127.0.0.1:8000`
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
+    share: true,
+    queryParams: { username },
+  })
+
+  const THROTTLE = 10
+  const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
+
+  useEffect(() => {
+    sendJsonMessage({
+      x: 0,
+      y: 0,
     })
-  
-    const THROTTLE = 10
-    const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
-  
-    useEffect(() => {
-      sendJsonMessage({
-        x: 0,
-        y: 0,
+    window.addEventListener("mousemove", (e) => {
+      sendJsonMessageThrottled.current({
+        x: e.clientX,
+        y: e.clientY,
       })
-      window.addEventListener("mousemove", (e) => {
-        sendJsonMessageThrottled.current({
-          x: e.clientX,
-          y: e.clientY,
-        })
-      })
-    }, [])
-  
-    if (lastJsonMessage) {
-      return <>{renderCursors(lastJsonMessage)}</>
-    }
+    })
+  }, [])
+
+  if (lastJsonMessage) {
+    return <>{renderCursors(lastJsonMessage)}</>
+  }
 }
+
+
+
+
